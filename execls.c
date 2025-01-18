db973841b3c82264d921ccd9e549637b37e54530
Arjun-MS2005 <arjunms2005@gmail.com>
2025-01-18 13:24:58 +0530

    Added general command execution with execvp and better input formatting

--- execls.c
+++ execls.c
@@ -5,17 +5,27 @@
 #include <string.h>
 
 #define MAXCOMMANDSIZE 100
+#define MAXARGS 100
 
-void execls() {
+void parseCommand(char *command, char *args[]) {
+    int i = 0;
+    char *token = strtok(command, " ");
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+}
+
+void executeCommand(char *args[]) {
     pid_t pid = fork();
-
     if (pid > 0) {
-        printf("Executing ls\n");
+        printf("Executing Command...\n");
         wait(NULL);
     } else if (pid == 0) {
         printf("Child process running...\n");
-        execlp("ls", "ls", "-l", NULL);
-        perror("execlp failed");
+        execvp(args[0], args);
+        perror("execvp failed");
         exit(EXIT_FAILURE);
     } else {
         perror("fork failed");
@@ -23,16 +33,33 @@ void execls() {
     }
 }
 
+void trimWhitespace(char *str) { // function to trim leading and trailing white spaces
+    int start = 0, end = strlen(str) - 1;
+
+    while (str[start] == ' ' || str[start] == '\t') {
+        start++;
+    }
+
+    while (end >= start && (str[end] == ' ' || str[end] == '\t')) {
+        end--;
+    }
+
+    for (int i = start; i <= end; i++) {
+        str[i - start] = str[i];
+    }
+    str[end - start + 1] = '\0';
+}
+
 int main() {
     int flag = 1;
+    char command[MAXCOMMANDSIZE];
+    char *args[MAXARGS];
 
     printf("WELCOME TO MY COMMAND LINE\n");
     while (flag) {
         printf(">>(type 'exit' to quit): ");
-        char command[MAXCOMMANDSIZE];
         if (fgets(command, MAXCOMMANDSIZE, stdin) != NULL) {
             size_t len = strlen(command);
-            // Remove the newline character if present
             if (len > 0 && command[len - 1] == '\n') {
                 command[len - 1] = '\0';
             }
@@ -41,13 +68,14 @@ int main() {
             continue;
         }
 
-        if (strcmp(command, "ls") == 0) {
-            execls();
-        } else if (strcmp(command, "exit") == 0) {
+        trimWhitespace(command); 
+
+        if (strcmp(command, "exit") == 0) {
             printf("Thank you for trying this out!\n");
             flag = 0;
         } else {
-            printf("Command '%s' is not supported.\n", command);
+            parseCommand(command, args);
+            executeCommand(args);
         }
     }
 
