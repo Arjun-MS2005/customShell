5db239b6dfe9efad07a8cb2c0c0e6981308b346c
Arjun-MS2005 <arjunms2005@gmail.com>
2025-01-18 12:02:18 +0530

    added basic input loop

--- execls.c
+++ execls.c
@@ -2,39 +2,54 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
 
-void execls(){
+#define MAXCOMMANDSIZE 100
+
+void execls() {
     pid_t pid = fork();
 
-    if(pid > 0){
-        printf("Executing ls");
+    if (pid > 0) {
+        printf("Executing ls\n");
         wait(NULL);
-    }
-    else if(pid == 0){
-        printf("child process running...\n");
-        execlp("ls" , "ls" , "-l", NULL);
+    } else if (pid == 0) {
+        printf("Child process running...\n");
+        execlp("ls", "ls", "-l", NULL);
         perror("execlp failed");
         exit(EXIT_FAILURE);
-    }
-    else{
-        printf("fork failed");
+    } else {
+        perror("fork failed");
         exit(EXIT_FAILURE);
     }
-
 }
 
+int main() {
+    int flag = 1;
 
-
-int main(){
     printf("WELCOME TO MY COMMAND LINE\n");
-    printf("DO YOU WANT TO EXECUTE LS(Y/N)?\n");
-    char choice;
-    scanf(" %c" , &choice);
-    if (choice == 'Y' || choice == 'y'){
-        execls();
-    }
-    else{
-        exit(1);
+    while (flag) {
+        printf(">>(type 'exit' to quit): ");
+        char command[MAXCOMMANDSIZE];
+        if (fgets(command, MAXCOMMANDSIZE, stdin) != NULL) {
+            size_t len = strlen(command);
+            // Remove the newline character if present
+            if (len > 0 && command[len - 1] == '\n') {
+                command[len - 1] = '\0';
+            }
+        } else {
+            printf("Error reading input\n");
+            continue;
+        }
+
+        if (strcmp(command, "ls") == 0) {
+            execls();
+        } else if (strcmp(command, "exit") == 0) {
+            printf("Thank you for trying this out!\n");
+            flag = 0;
+        } else {
+            printf("Command '%s' is not supported.\n", command);
+        }
     }
 
+    return 0;
 }
